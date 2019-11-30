4ee794eb53fbf0da06a481864c41f3b4b54a752f
sonnybardhan <sonnybardhan@github.io>
2019-11-30 22:40:51 +0530

    Updates

---
+++ precache-manifest.1315bda370117b5c1b3bcb95b144074d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89d85a6fd710e719f0c60522e53934c8",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "d1da53c7fd6f638f7381",
+    "url": "/monsters/static/css/main.378ae639.chunk.css"
+  },
+  {
+    "revision": "450602fe23d1598b5423",
+    "url": "/monsters/static/js/2.0b47c197.chunk.js"
+  },
+  {
+    "revision": "d1da53c7fd6f638f7381",
+    "url": "/monsters/static/js/main.ba2182d4.chunk.js"
+  },
+  {
+    "revision": "eea957b2da93fe7d5801",
+    "url": "/monsters/static/js/runtime-main.1c4ac48d.js"
+  }
+]);
